168b244b2d5b278792c9ad0051b3f4e67e97eefc
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2018-02-16 15:19:42 +0000

    update autotooling

---
+++ libloc.c
@@ -0,0 +1,5 @@
+char *libloc=LIBLOC;		/* "/usr/local/lib/logo/logolib" */
+char *helploc=HELPLOC;		/* "/usr/local/lib/logo/helpfiles" */
+char *cslsloc=CSLSLOC;		/* "/usr/local/lib/logo/csls" */
+char *temploc=TEMPLOC;		/* "/tmp" */
+char *separator="/";
