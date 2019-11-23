9cff4293db4c7322327e2acca63aa5b4ac3d5668
Joshua J. Cogliati <jrincayc@yahoo.com>
2019-11-22 21:37:09 -0700

    Removing XXX since 0 or 1 probably both work.

--- eval.c
+++ eval.c
@@ -677,7 +677,7 @@ compound_apply:
     check_ibm_stop();
 #endif
 #ifdef HAVE_WX
-    check_wx_stop(0); //XXX: Not sure what the value should be
+    check_wx_stop(0);
 #endif
     if ((tracing = flag__caseobj(fun, PROC_TRACED))) {
 	for (i = 0; i < trace_level; i++) print_space(writestream);
