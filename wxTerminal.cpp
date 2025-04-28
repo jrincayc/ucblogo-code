2d4140cc77d971f64474c616edb72a876fb74a54
1bildung <66473636+1bildung@users.noreply.github.com>
2025-04-28 03:59:47 +0200

    Enable AltGr in wxTerminal. (#218)
    * Modify checking for keyboard modifiers to enable AltGr in wxTerminal.
    
    ---------
    
    Co-authored-by: m_b <service@pop-os.localdomain>
    Co-authored-by: Joshua J. Cogliati <jrincayc@yahoo.com>

--- wxTerminal.cpp
+++ wxTerminal.cpp
@@ -1156,11 +1156,14 @@ extern "C" void do_keyact(int);
 void
 wxTerminal::OnChar(wxKeyEvent& event)
 {
-  if(event.HasModifiers()) {
-    //If the key event has control or alt pressed, let something else handle it
+  //If the key event has control or alt pressed, let something else handle it
+  int modCode = (int) event.GetModifiers();
+  if((modCode == wxMOD_ALT) || (modCode == wxMOD_CONTROL)
+     || (modCode == (wxMOD_CONTROL | wxMOD_SHIFT))) {
     event.Skip();
     return;
   }
+
   ClearSelection();
   int
       keyCode = 0,
